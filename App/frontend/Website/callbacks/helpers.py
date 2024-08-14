1b6e8f37c3c536c3749138bff7462f84c9839353
Romano <82478812+RomanoGameplay@users.noreply.github.com>
2024-08-13 21:12:03 -0300

    adding rolling means in the graph

--- App/frontend/Website/callbacks/helpers.py
+++ App/frontend/Website/callbacks/helpers.py
@@ -27,7 +27,7 @@ def gen_graph(df, fig, graph_type):
     elif graph_type == 'Line':
         fig.add_trace(go.Scatter(x=df['Date'],
                                  y=df['Close'],
-                                 line=dict(color='rgba(255, 255, 255, 0.9)', width=1),
+                                 line=dict(color='rgba(255, 255, 255, 1)', width=2),
                                  mode='lines',
                                  connectgaps=True,
                                  hovertemplate='price: %{y}<extra></extra>',
@@ -46,8 +46,38 @@ def gen_graph(df, fig, graph_type):
                   secondary_y=False)
     fig.update_yaxes(secondary_y=True, showgrid=True)
     fig.update_yaxes(secondary_y=False, showgrid=False, showticklabels=False)
+    rolling_mean_df = calculate_rolling_mean(df)
+    fig = gen_rolling_mean_graphs(rolling_mean_df=rolling_mean_df, fig=fig)
 
     return fig
+
+
+def gen_rolling_mean_graphs(rolling_mean_df, fig):
+    period_list = (15, 25, 72)
+    alpha = 0.9
+    colors_lines = (f'rgba(255, 130, 0, {alpha})', f'rgba(0, 0, 255, {alpha})', f'rgba(255, 0, 255, {alpha})')
+    term_list = ('Short', 'Medium', 'Long')
+    for i in range(len(period_list)):
+        hovertemplate_str = f'{term_list[i]} Term: '
+        fig.add_trace(go.Scatter(x=rolling_mean_df['Date'],
+                                 y=rolling_mean_df[f'{period_list[i]}_days_RM'],
+                                 connectgaps=True,
+                                 hovertemplate=hovertemplate_str + '%{y}<extra></extra>',
+                                 mode='lines',
+                                 line=dict(color=colors_lines[i], width=1),
+                                 name=f'{term_list[i]} ({period_list[i]} days)'),
+                      secondary_y=True)
+    fig.update_layout(legend_title_text='Rolling Mean Term')
+    return fig
+
+
+def calculate_rolling_mean(df):
+    periods_list = (15, 25, 72)
+    new_columns = [pl.col('Date')]
+    for period in periods_list:
+        new_columns.append(pl.col('Close').rolling_mean(window_size=period).alias(f'{period}_days_RM'))
+    rolling_mean_df = df.select(new_columns)
+    return rolling_mean_df
 
 
 def format_graph(fig):
@@ -87,7 +117,6 @@ def format_big_numbers(nums):
             formatted_num = num / (1000 ** magnitude)
             result.append(f'{formatted_num:.2f}{suffixes[magnitude]}')
     return result
-
 
 # if __name__ == '__main__':
 # print(gen_table('AAPL'))
